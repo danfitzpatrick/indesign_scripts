b82894d4dcfd9c7ced11dbe0171d0c0354eabf83
danfitzpatrick <dan.fpatrick@gmail.com>
2014-08-28 12:49:44 -0700

    Added new script

--- report_missing_links.jsx
+++ report_missing_links.jsx
@@ -1,16 +1,26 @@
-////////////////////////////////////////////
-// Set preferences for packaging here
+/*
+
+I wrote (see functions at the bottom for credits to other authors' work) this script as a way of quickly opening up each 
+InDesign file in a folder and its subfolders, checking for missing links and then logging those missing
+links to two files.
+
+The first log file itemizes what was found in each file, and the path where the missing link "should" be.
+
+The second log makes a list of missing files by name only, for use by a second script which can search
+entire hard drives for those missing links.
+
+--Dan Fitzpatrick
+
+*/
 
 app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
 
-var g = {};
-
-chooseFolders();
-
+var g = {}; 
 var myFiles = [];
 var wontOpenFiles = [];
 var missingLinkFiles = [];
 
+chooseFolders();
 GetSubFolders(g.sourceFolder);
 
 myReportFolder = g.sourceFolder.name.split("/",1);
@@ -23,83 +33,70 @@ var myLinksLogPath = File(g.destinationFolder + "/" + myReportFolder + "-- missi
 var myLog = makeLogFile(myLogPath,true);
 var myLinksLog = makeLogFile(myLinksLogPath,true);
 
-
 var currentdate = new Date();
 var datetime = "Generated " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " at "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+    + (currentdate.getMonth()+1)  + "/" 
+    + currentdate.getFullYear() + " at "  
+    + currentdate.getHours() + ":"  
+    + currentdate.getMinutes() + ":" 
+    + currentdate.getSeconds();
                               
-	log(myLogPath,myReportFolder + " Missing Files Report\r");
-	log(myLogPath,datetime);
-	log(myLogPath,"\r\r");
+log(myLogPath,myReportFolder + " Missing Files Report\r");
+log(myLogPath,datetime);
+log(myLogPath,"\r\r");
 
 var myFilesLength = myFiles.length;
-	log(myLogPath,"Script found " + myFilesLength + " Indesign Files\r\r");
+log(myLogPath,"Script found " + myFilesLength + " Indesign Files\r\r");
 
-app.scriptPreferences.userInteractionLevel = UserInteractionLevels.NEVER_INTERACT;
-
-	var myMissingSummary = [];
+var myMissingSummary = [];
 
 for(i=0; i<myFilesLength; i++) {
+	try { 
+		var myDocument = app.open(File(myFiles[i])) 
+			var myLinks = myDocument.links;
+			var numLinks = myDocument.links.length;	
+			log(myLog, "\rFile " + (i+1).toString() + " of " + myFilesLength + " -- " + myDocument.name +"\r")
 
-
-try { 
-	var myDocument = app.open(File(myFiles[i])) 
-		var myLinks = myDocument.links;
-		var numLinks = myDocument.links.length;
-		
-		log(myLog, "\rFile " + (i+1).toString() + " of " + myFilesLength + " -- " + myDocument.name +"\r")
-
-
-	for(var p=0;p<numLinks;p++) {
-
-		if(myLinks[p].status==1819109747) {
-// 		log(myLog, myFiles[i] + "\r" );
-		log(myLog, "\t*** Link MISSING *** " + myLinks[p].filePath + "\r")
-			myMissingSummary.push(myLinks[p].filePath)
-		myLog.execute();
-
+		for(var p=0;p<numLinks;p++) {
+			if(myLinks[p].status==1819109747) {
+			log(myLog, "\t*** Link MISSING *** " + myLinks[p].filePath + "\r")
+				myMissingSummary.push(myLinks[p].filePath)
+			myLog.execute();
+			}		
 		}
-		
-// 		if(myLinks[p].status==1819242340) {
-// 		log(myLog, "\t\t * Link Modified * " + myLinks[p].filePath + "\r")
-// 		
-// 		}
+			myDocument.close();
 
-	}
+		} catch(e) {
+			log(myLog, "\rFile " + (i+1).toString() + " of " + myFilesLength)
+			log(myLog, "\r" + myFiles[i] + "\r" );
+			log(myLog,"\t*** ERROR: File Did Not Open\r\r");	
+		}
 
-		myDocument.close();
-
-	} catch(e) {
-		log(myLog, "\rFile " + (i+1).toString() + " of " + myFilesLength)
-		log(myLog, "\r" + myFiles[i] + "\r" );
-		log(myLog,"\t*** ERROR: File Did Not Open\r\r");
-	
-	}
-
-myLog.execute();
-
+	myLog.execute();
 }
 
 myMissingSummary = eliminateDuplicates(myMissingSummary);
 
-		log(myLog, "\r\rSummary of Missing Files\r" );
-
+log(myLog, "\r\rSummary of Missing Files\r" );
 for(k=0;k<myMissingSummary.length;k++){
-		log(myLog, myMissingSummary[k] +"\r")
+	log(myLog, myMissingSummary[k] +"\r")
 }
 
 for(r=0;r<myMissingSummary.length;r++){
-		log(myLinksLog, myMissingSummary[r] +"\r")
+	log(myLinksLog, myMissingSummary[r] +"\r")
 }
 
-		log(myLog, "\r\r** End of Script **\r" );
+log(myLog, "\r\r** End of Script **\r" );
 
 myLog.execute();
 
+
+///////////////////
+//// FUNCTIONS ////
+///////////////////
+
+// Credit to Raphael Montanaro
+// http://stackoverflow.com/users/103281/raphael-montanaro
 function eliminateDuplicates(arr) {
   var i,
       len=arr.length,
@@ -115,8 +112,9 @@ function eliminateDuplicates(arr) {
   return out;
 }
 
-////////////// Log - Make the Log File 
-function makeLogFile(aName, deleteIt) {
+// Make a Log File -- from Dave Saunders
+// http://jsid.blogspot.com/2006/04/logging-to-text-file.html
+function makeLogFile(aName, deleteIt) { 
   var aFile = File(aName);
   	aFile.encoding="UTF-8";
 
@@ -129,7 +127,8 @@ function makeLogFile(aName, deleteIt) {
   	return aFile
 }
 
-////////////// Log - Make the Log Entry
+// Make a Log Entry -- from Dave Saunders
+// http://jsid.blogspot.com/2006/04/logging-to-text-file.html
 function log(aFile, message) {
   if (!aFile.exists) {
     aFile.open("w");
@@ -141,8 +140,9 @@ function log(aFile, message) {
   aFile.close();
 }
 
-
-////////////// Choose Folder Function
+// Choose Folder Function by Grant Gamble 
+// http://gamblecomputersolutions.co.uk/
+// http://amzn.com/1460915380
 function chooseFolders(){
 	g.sourceFolder = Folder.selectDialog("Please select the root folder.");
 	if(g.sourceFolder == null){return false;}
@@ -160,7 +160,8 @@ function chooseFolders(){
 	}
 }
 
-////////////// Get Files Recursively From Folder 
+// Get Files Recursively From Folder
+// by Kasyan Servetsky http://www.kasyan.ho.com.ua/
 function GetSubFolders(theFolder) {
      var myFileList = theFolder.getFiles();
      for (var i = 0; i < myFileList.length; i++) {
@@ -174,7 +175,9 @@ function GetSubFolders(theFolder) {
      }
 }
 
-function VerifyFolder(myFolder) {
+// by Bob Stucky creativescripting.net and Kasyan 
+// Kasyan Servetsky http://www.kasyan.ho.com.ua/
+function VerifyFolder(myFolder) { 
 	if (!myFolder.exists) {
 		var myFolder = new Folder(myFolder.absoluteURI);
 		var myArray1 = new Array();
